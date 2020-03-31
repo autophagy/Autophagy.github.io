1a40143ad5bbe68d7ac0350a9affea103656631e
Autophagy <mika@crate.io>
2020-03-31 21:28:56 +0200

    New landing

---
+++ precache-manifest.129fc36cc3495311014de80a5418b49e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151313d40b3052190ed3c92d2d2f0a22",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "283b0d700e260e21b0161f7eb32c5ddb",
+    "url": "/fish.png"
+  },
+  {
+    "revision": "34cd1f173832023c47a0d6d3b8a1459e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ea73559106147d63b22caa6c4b64683",
+    "url": "/index.png"
+  },
+  {
+    "revision": "b93e7acd4ddc12d075d2fbb1598c260a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "434eac8b4f2d1a260bae",
+    "url": "/static/css/main.54711a04.chunk.css"
+  },
+  {
+    "revision": "434eac8b4f2d1a260bae",
+    "url": "/static/js/main.434eac8b.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "604c6712e7dcf2851e2c",
+    "url": "/static/js/vendors~main.604c6712.chunk.js"
+  }
+]);
